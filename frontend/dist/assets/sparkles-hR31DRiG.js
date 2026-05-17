1dce40971a4ba6b2de1b6862500490d9a803a13f
hoodhahmed <96528096+hoodhahmed@users.noreply.github.com>
2026-05-17 08:35:06 +0000

    render#4

---
+++ frontend/dist/assets/sparkles-hR31DRiG.js
@@ -0,0 +1,26 @@
+import{r as a}from"./index-DaaS0gJ0.js";/**
+ * @license lucide-react v0.368.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var u={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.368.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase();/**
+ * @license lucide-react v0.368.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=(e,l)=>{const o=a.forwardRef(({color:c="currentColor",size:t=24,strokeWidth:s=2,absoluteStrokeWidth:p,className:h="",children:r,...k},d)=>a.createElement("svg",{ref:d,...u,width:t,height:t,stroke:c,strokeWidth:p?Number(s)*24/Number(t):s,className:["lucide",`lucide-${w(e)}`,h].join(" "),...k},[...l.map(([i,m])=>a.createElement(i,m)),...Array.isArray(r)?r:[r]]));return o.displayName=`${e}`,o};/**
+ * @license lucide-react v0.368.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const C=n("Check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);/**
+ * @license lucide-react v0.368.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const L=n("Sparkles",[["path",{d:"m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z",key:"17u4zn"}],["path",{d:"M5 3v4",key:"bklmnn"}],["path",{d:"M19 17v4",key:"iiml17"}],["path",{d:"M3 5h4",key:"nem4j1"}],["path",{d:"M17 19h4",key:"lbex7p"}]]);export{C,L as S,n as c};
